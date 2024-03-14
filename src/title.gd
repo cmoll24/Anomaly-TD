050d1c85cb99b9623df00354b846c6dc8e7fee2b
Christophe Moll <115204696+cmoll24@users.noreply.github.com>
2024-03-14 00:54:52 -0700

    Revert "Revert "Final game jam version""
    This reverts commit b4557a852cfc04fc9b4db55d8cb10f75c4e6e97f.

--- src/title.gd
+++ src/title.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@onready var highscore = $Highscore
+
+func _ready():
+	highscore.text += str(GlobalVariables.get_highscore())
 
 func _on_level_1_pressed():
 	var _error = get_tree().change_scene_to_file("res://src/level1.tscn")
